c9f2e951c36dd898e2586a2154e9554670306e74
tecnico2 <tecnico2@minervadigitalintelligence.com>
2022-11-22 17:28:01 +0100

    add timeLine

--- src/componets/Main.js
+++ src/componets/Main.js
@@ -10,6 +10,7 @@ import MainContent from "./Pages/MainPage/MainContent";
 import WeekInfo from "./Pages/ChildPage/Cards/WeekInfo";
 import ChildMeasures from "./Pages/ChildPage/Cards/ChildMeasures";
 import MyWeight from "./Pages/ChildPage/Cards/MyWeight";
+import TimeLine from "./Pages/ChildPage/Cards/TimeLine";
 
 const Main = () => {
     return (
@@ -18,6 +19,7 @@ const Main = () => {
                 <Route path="/info" element={<WeekInfo />} />
                 <Route path="/baby-measures" element={<ChildMeasures />} />
                 <Route path="/my-weight" element={<MyWeight />} />
+                <Route path="/timeline" element={<TimeLine />} />
                 <Route path="/" element={<MainContent />} />
                 <Route path="/baby" element={<ChildPage />} />
                 <Route path="/guide" element={<ChronologyPage />} />
---
+++ src/componets/Pages/ChildPage/Cards/TimeLine.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./styles/WeekInfo.module.scss";
+import { IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HearingIcon from "@mui/icons-material/Hearing";
+import RedeemIcon from "@mui/icons-material/Redeem";
+import s from "./styles/TimeLine.module.scss";
+
+const numbers = [];
+for (let i = 1; i < 43; i++) {
+    numbers.push(i);
+}
+const TimeLine = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className={styles.weekInfoBlock}>
+                <div className={styles.goBackButton}>
+                    <IconButton onClick={() => navigate(-1)}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className={s.mainTimeLineBlock}>
+                <div className={s.numbers}>
+                    {numbers.map((m) => (
+                        <div className={s.number} key={m}>
+                            {m}
+                        </div>
+                    ))}
+                    <div className={s.line1}>
+                        Esami del sangue per animia falciforme e talassemia
+                    </div>
+                    <div className={s.line2}>Esame per gli anticorpi</div>
+                    <div className={s.line3}>Test anamalie cromosomiche</div>
+                    <div className={s.line4}>Translucenza nucale</div>
+                    <div className={s.line5}>Ecografia per la dotazione</div>
+                    <div className={s.line6}>1 ecografia</div>
+                    <div className={s.line7}>Test di gravidanza</div>
+                    <div className={s.line8}>Integratore di acido folico</div>
+                    <div className={s.line9}>Ecografia morfologica</div>
+                    <div className={s.line10}>Ecografia per rilevare il sesso</div>
+                    <div className={s.line11}>Si prepara la cameretta</div>
+                    <div className={s.line12}>Piano del parto</div>
+                    <div className={s.line13}>La borsa per ospedale</div>
+                    <div className={s.dot1}>
+                        <FavoriteIcon className={s.icon} />
+                        Il cuore del bambino inizia a battere
+                    </div>
+                    <div className={s.vl}></div>
+                    <div className={s.dot2}>
+                        <HearingIcon className={s.icon} />
+                        Il bambino recunosce le voci
+                    </div>
+                    <div className={s.vl2}></div>
+                    <div className={s.dot3}>
+                        <RedeemIcon className={s.icon} />
+                        Termine
+                    </div>
+                    <div className={s.vl3}></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TimeLine;
